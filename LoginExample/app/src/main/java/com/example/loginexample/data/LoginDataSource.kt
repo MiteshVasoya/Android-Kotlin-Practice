ec22a8ee546b32d09f9fdc5fb0cf4a56af03e801
Mitesh Vasoya <miteshvasoya@Miteshs-MacBook-Pro.local>
2019-09-24 14:03:25 -0400

    Login example added

---
+++ LoginExample/app/src/main/java/com/example/loginexample/data/LoginDataSource.kt
@@ -0,0 +1,28 @@
+package com.example.loginexample.data
+
+import android.content.Intent
+import androidx.core.content.ContextCompat.startActivity
+import com.example.loginexample.HomepageActivity
+import com.example.loginexample.data.model.LoggedInUser
+import java.io.IOException
+
+/**
+ * Class that handles authentication w/ login credentials and retrieves user information.
+ */
+class LoginDataSource {
+
+    fun login(username: String, password: String): Result<LoggedInUser> {
+        try {
+            // TODO: handle loggedInUser authentication
+            val fakeUser = LoggedInUser(java.util.UUID.randomUUID().toString(), "Jane Doe")
+            return Result.Success(fakeUser)
+        } catch (e: Throwable) {
+            return Result.Error(IOException("Error logging in", e))
+        }
+    }
+
+    fun logout() {
+        // TODO: revoke authentication
+    }
+}
+
