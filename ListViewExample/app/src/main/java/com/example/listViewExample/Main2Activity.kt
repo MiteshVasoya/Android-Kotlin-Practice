5e8d0bfc7d4bcaf9b248dcc0ec849960e7f530fe
Mitesh Vasoya <miteshvasoya@Miteshs-MacBook-Pro.local>
2019-09-24 00:54:56 -0400

    Refresh Listview

--- ListViewExample/app/src/main/java/com/example/listViewExample/Main2Activity.kt
+++ ListViewExample/app/src/main/java/com/example/listViewExample/Main2Activity.kt
@@ -2,11 +2,9 @@ package com.example.listViewExample
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
-import android.widget.ListView
-import android.widget.Toast
+import android.widget.*
 
 class Main2Activity : AppCompatActivity() {
 
@@ -14,7 +12,7 @@ class Main2Activity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
 
-        var array = arrayOf("1", "2", "3", "4", "5")
+        var array = arrayOf("5", "4", "3", "2", "1")
 
         var adapter = ArrayAdapter(this, R.layout.listview_item, array)
         val listView:ListView = findViewById(R.id.listview)
@@ -30,6 +28,24 @@ class Main2Activity : AppCompatActivity() {
                 val itemValue = listView.getItemAtPosition(position) as String
                 Toast.makeText(applicationContext, "$itemValue", Toast.LENGTH_SHORT).show()
             }
+        }
+
+        val changeButton:Button = findViewById(R.id.changeButton)
+        val refreshButton:Button = findViewById(R.id.refreshButton)
+
+        var i = 6;
+
+        changeButton.setOnClickListener{
+            array.set(0, ""+i++)
+
+            Log.d("Main2Activity", "array:")
+            for (element in array) {
+                println(element)
+            }
+        }
+
+        refreshButton.setOnClickListener{
+            adapter.notifyDataSetChanged()
         }
     }
 }
